1d82093081ccb7dbfef4187614af9438150824f0
Daniela-Prada <d.prada1742@uniandes.edu.co>
2024-10-14 17:27:15 -0500

    Add: add main files

---
+++ app/main.py
@@ -0,0 +1,58 @@
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
+
+from .routers import company, user
+from .errors.errors import ApiError
+
+app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#app.include_router(company.router)
+
+version = "1.0"
+
+@app.get("/incident-management/health")
+async def health():
+    return {"status": "OK"}
+
+@app.exception_handler(ApiError)
+async def api_error_exception_handler(request: Request, exc: ApiError):
+    return JSONResponse(
+        status_code=exc.code,
+        content={
+            "mssg": exc.description, 
+            "details": str(exc),
+            "version": version
+        },
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = []
+    for error in exc.errors():
+        error_detail = {
+            "location": error["loc"],
+            "message": error["msg"],
+            "type": error["type"]
+        }
+        errors.append(error_detail)
+
+    return JSONResponse(
+        status_code=400,
+        content={
+            "message": "Validation Error",
+            "details": errors,
+            "version": version
+        },
+    )
