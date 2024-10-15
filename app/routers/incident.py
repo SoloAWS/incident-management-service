c99ef016342e0cf74fe7dd0fbe58cab07be677d3
Daniela-Prada <d.prada1742@uniandes.edu.co>
2024-10-14 19:10:30 -0500

    update: removed proxy endpoint

--- app/routers/incident.py
+++ app/routers/incident.py
@@ -7,8 +7,7 @@ import os
 
 router = APIRouter(prefix="/incident-management", tags=["Incidents"])
 
-# URL of the microservice that actually accesses the database
-DATABASE_SERVICE_URL = os.getenv("DATABASE_SERVICE_URL", "http://localhost:8003/incident")
+INCIDENT_SERVICE_COMMAND_URL = os.getenv("INCIDENT_SERVICE_COMMAND_URL", "http://localhost:8004/command-incident")
 
 SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'secret_key')
 ALGORITHM = "HS256"
@@ -20,22 +19,8 @@ def get_current_user(token: str):
     except jwt.PyJWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-def get_incidents_from_database(user_id: str, company_id: str, token: str):
-    api_url = DATABASE_SERVICE_URL
-    endpoint = "/user-incidents"
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "user_id": user_id,
-        "company_id": company_id
-    }
-    response = requests.post(f"{api_url}{endpoint}", headers=headers, json=data)
-    return response.json(), response.status_code
-
 def create_incident_in_database(incident_data: dict, token: str):
-    api_url = DATABASE_SERVICE_URL
+    api_url = INCIDENT_SERVICE_COMMAND_URL
     endpoint = "/"
     headers = {
         "Authorization": f"Bearer {token}",
@@ -44,24 +29,7 @@ def create_incident_in_database(incident_data: dict, token: str):
     response = requests.post(f"{api_url}{endpoint}", headers=headers, json=incident_data)
     return response.json(), response.status_code
 
-@router.post("/user-incidents", response_model=IncidentsResponse)
-async def get_user_incidents(
-    request: UserCompanyRequest,
-   # current_user: dict = Depends(get_current_user)
-):
-    #token = jwt.encode(current_user, SECRET_KEY, algorithm=ALGORITHM)
-    incidents_data, status_code = get_incidents_from_database(
-        str(request.user_id), 
-        str(request.company_id), 
-        'token'
-    )
-    
-    if status_code != 200:
-        raise HTTPException(status_code=status_code, detail=incidents_data)
-    
-    return IncidentsResponse(**incidents_data)
-
-@router.post("/incidents", response_model=CreateIncidentResponse)
+@router.post("/", response_model=CreateIncidentResponse)
 async def create_incident(
     incident: CreateIncidentRequest,
     #current_user: dict = Depends(get_current_user)
